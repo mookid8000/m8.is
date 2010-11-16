7322d130e9de73b66ee4f9227ff8e57ee560c0e4
Mogens Heller Grabe <mookid8000@gmail.com>
2010-11-16 22:05:52 +0100

    app now adds test doc to mongo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-console.log('Starting Rafiki!');
-
-console.log('Setting up vars');
-
 var server_port = 8000;
 
 var mongodb_host = 'localhost',
@@ -24,6 +20,23 @@ db = new mongo.Db(mongodb_db_name, new mongo.Server(mongodb_host, mongodb_port, 
 
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.post('/shortify', function(req, res) {
+	var url = req.body['url'];
+	console.log('url: ' + url);
+	
+	var doc = {
+		url: url,
+	};
+	
+	db.open(function() {
+		db.collection('urls', function (err, coll) {
+			coll.insert(doc, function() {
+				res.redirect('back');
+			});
+		})
+	})
 });
 
 console.log("Listening...");
