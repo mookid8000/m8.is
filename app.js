4942707c5c28cbc0470b321ff37f63017a793e96
Mogens Heller Grabe <mookid8000@gmail.com>
2011-01-19 18:27:15 +0100

    moved modules into modules folder

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var mongodb_host = 'localhost',
 
 var express = require('express');
 var util = require('util');
-var repos = require('./repos');
-var md5 = require('./md5')
+var repos = require('./include/repos');
+var md5 = require('./include/md5')
 var connect_mongodb = require('connect-mongodb');
 
 repos.initialize(mongodb_host, mongodb_port, mongodb_db_name);
