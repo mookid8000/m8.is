0151ab70d4d1a3534517f61cc4f1537b90fbe1b2
Mogens Heller Grabe <mookid8000@gmail.com>
2011-01-19 22:01:09 +0100

    allowed for configuring the app via the command line

--- app.js
+++ app.js
@@ -3,21 +3,112 @@ String.prototype.startsWith = function(str)
 	return (this.match("^"+str) == str);
 }
 
-var server_port = 8164;
+var args = process.argv;
 
-var base_url = 'http://localhost:' + server_port;
+/* Settings ------------------------------------------- */
 
-var mongodb_host = 'localhost',
-	mongodb_port = 27017,
-	mongodb_db_name = 'm8_development';
+var server_port = args[2] ? parseInt(args[2]) : 8000;
+var server_host = args[3] ? args[3] : 'localhost';
 
+var mongodb_host = args[4] ? args[4] : 'localhost',
+	mongodb_port = args[5] ? parseInt(args[5]) : 27017,
+	mongodb_db_name = args[6] ? args[6] : 'm8_development',
+	mongodb_user = args[7] ? args[7] : null,
+	mongodb_pwd = args[8] ? args[8] : null;
+
+/* ---------------------------------------------------- */
+
+var util = require('util');
+var repos = require('./modules/repos');
+var md5 = require('./modules/md5')
+var connect_mongodb = require('connect-mongodb');
 var express = require('express');
 
-var app2 = express.createServer();
-app2.get('/', function(req, res) {
-	res.render('index');
+var base_url = 'http://' + server_host + ':' + server_port;
+
+console.log('Configured base URL: ' + base_url);
+
+var app = express.createServer();
+
+var dump = function(obj) {
+	console.log(util.inspect(obj, false, null));
+}
+
+app.configure(function() {
+	app.set('view engine', 'haml');
+	app.use(express.staticProvider(__dirname + '/public'));
+	app.use(express.bodyDecoder());
+	app.use(express.cookieDecoder());
+	
+	var mongodb_session_store_config = function() {
+		var obj = {
+			dbname: mongodb_db_name,
+			host: mongodb_host,
+			port: mongodb_port
+		};
+		
+		if (mongodb_user) {
+			obj.username = mongodb_user;
+			obj.password = mongodb_pwd;
+		}
+		
+		return {
+			store: connect_mongodb(obj)
+		};
+	};
+	
+	app.use(express.session(mongodb_session_store_config()));
 });
-app2.listen(server_port);
 
+app.get('/', function(req, res) {
+	res.render('index', {locals: {
+		flash: req.flash(), 
+		urls: req.session.state ? req.session.state.urls.slice().reverse() : []
+	}});
+});
 
+app.post('/shortify', function(req, res) {
+	var body = req.body;
+	var url = body.url;
+	
+	if (!(url.startsWith('http://') || url.startsWith('https://'))) {
+		url = 'http://' + url;
+	}
+	
+	var key = md5.generate(url);
+	
+	var doc = {
+		url: url,
+		_id: key
+	};
 
+	repos.insert(doc, function() {
+		req.flash('info', 'URL _%s_ has been shortified...', url);
+		
+		var session = req.session;
+		
+		if (!session.state) session.state = {created: new Date(), urls: []};
+		
+		session.state.urls.push({url: url, link: base_url + '/r/' + key});
+		
+		res.redirect('back');
+	});
+});
+
+app.get('/r/:key', function(req, res) {
+	var key = req.params.key;
+	
+	repos.getByKey(key, function(url) {
+		res.redirect(url);
+	}, function() {
+		req.flash('error', 'Could not find shortified url for _%s_', key)
+		res.redirect('home')
+	});
+});
+
+repos.initialize(mongodb_host, mongodb_port, mongodb_db_name, mongodb_user, mongodb_pwd, function() {
+	console.log('Initialized MongoDB (' + mongodb_host + ':' + mongodb_port + '/' + mongodb_db_name + ')');
+
+	app.listen(server_port);
+	console.log("Listening on port " + server_port + "...");
+});
--- modules/repos.js
+++ modules/repos.js
@@ -1,4 +1,5 @@
 var mongo = require('mongodb');
+var util = require('util');
 var db;
 
 var assertInitialized = function() {
@@ -7,35 +8,52 @@ var assertInitialized = function() {
 	}	
 };
 
-exports.initialize = function(mongodb_host, mongodb_port, database_name) {
-	db = new mongo.Db(database_name, new mongo.Server(mongodb_host, mongodb_port, {auto_reconnect: true}), {});
+exports.initialize = function(mongodb_host, mongodb_port, database_name, username, password, initialized) {
+	var server = new mongo.Server(mongodb_host, mongodb_port, {auto_reconnect: true});
+	db = new mongo.Db(database_name, server, {});
+
+	db.addListener("error", function(err) { 
+		console.log(err);
+	}); 
+
+	db.open(function(err) {
+		if (!username) {
+			initialized();
+		}
+		else {
+			db.authenticate(username, password, function(err) {
+				initialized();
+			});
+	 	}
+	});
 };
 
-exports.insert = function(doc, inserted) {
+exports.insert = function(doc, inserted, onError) {
 	assertInitialized();
 	
-	db.open(function() {
-		db.collection('urls', function (err, coll) {
+	db.collection('urls', function (err, coll) {
+		if (err) {
+			onError(err);
+		}
+		else {
 			coll.insert(doc, function() {
 				inserted();
 			});
-		})
+		}
 	})
 };
 
 exports.getByKey = function(key, gotten, notGotten) {
 	assertInitialized();
 	
-	db.open(function() {
-		db.collection('urls', function (err, coll) {
-			coll.findOne({k: key}, function(err, doc) {
-				if (typeof doc == 'undefined') {
-					notGotten();
-				}
-				else {
-					gotten(doc.url);
-				}
-			});
-		})
+	db.collection('urls', function (err, coll) {
+		coll.findOne({k: key}, function(err, doc) {
+			if (typeof doc == 'undefined') {
+				notGotten();
+			}
+			else {
+				gotten(doc.url);
+			}
+		});
 	})
 };
